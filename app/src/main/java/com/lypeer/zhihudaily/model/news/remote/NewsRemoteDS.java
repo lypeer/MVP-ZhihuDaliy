fa157bb84e7fa34f7bb57e25d85d0633b78e093e
lypeer <luoyanglizi@outlook.com>
2017-02-15 18:45:24 +0800

    去除NetUtil，将其职责并入RemoteDS

--- app/src/main/java/com/lypeer/zhihudaily/model/news/remote/NewsRemoteDS.java
+++ app/src/main/java/com/lypeer/zhihudaily/model/news/remote/NewsRemoteDS.java
@@ -1,11 +1,19 @@
 package com.lypeer.zhihudaily.model.news.remote;
 
-import com.lypeer.zhihudaily.model.news.NewsNetUtil;
+import com.lypeer.zhihudaily.App;
+import com.lypeer.zhihudaily.R;
+import com.lypeer.zhihudaily.api.ApiService;
+import com.lypeer.zhihudaily.model.RetrofitClient;
 import com.lypeer.zhihudaily.api.OnResultCallback;
 import com.lypeer.zhihudaily.model.news.NewsDataSource;
 import com.lypeer.zhihudaily.model.news.bean.LatestNewsResponse;
+import com.lypeer.zhihudaily.utils.Constants;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by lypeer on 2017/2/8.
@@ -26,6 +34,28 @@ public class NewsRemoteDS implements NewsDataSource {
 
     @Override
     public void loadLatestNews(final OnResultCallback<List<LatestNewsResponse.StoriesBean>> resultCallback) {
-        NewsNetUtil.loadLatestNews(resultCallback);
+        RetrofitClient.buildService(ApiService.class)
+                .loadLatestNews()
+                .enqueue(new Callback<LatestNewsResponse>() {
+                    @Override
+                    public void onResponse(Call<LatestNewsResponse> call, Response<LatestNewsResponse> response) {
+                        if (null == response || null == response.body()) {
+                            resultCallback.onFail(App.getAppContext().getString(R.string.error_not_available));
+                            return;
+                        }
+
+                        LatestNewsResponse latestNewsResponse = response.body();
+                        if (!latestNewsResponse.getStories().isEmpty()) {
+                            resultCallback.onSuccess(latestNewsResponse.getStories(), Constants.ResultCode.REMOTE);
+                        } else {
+                            resultCallback.onFail(App.getAppContext().getString(R.string.error_data_null));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LatestNewsResponse> call, Throwable t) {
+                        resultCallback.onFail(null == t ? App.getAppContext().getString(R.string.error_network) : t.getMessage());
+                    }
+                });
     }
 }
